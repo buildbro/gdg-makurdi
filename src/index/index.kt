cc9f81dab146b55b84a3a1e0cf36bd497a9be0ca
buildbro <apeapius@gmail.com>
2019-07-05 08:42:46 +0100

    Add More Tags

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,5 +1,7 @@
 package index
 
+import react.dom.b
+import react.dom.div
 import react.dom.h1
 import react.dom.render
 import kotlin.browser.document
@@ -7,8 +9,17 @@ import kotlin.browser.document
 fun main(args: Array<String>) {
     val rootDiv = document.getElementById("root")
     render(rootDiv) {
-        h1 {
-            + "Hello World!"
+        div {
+            h1 {
+                + "Blackjack"
+            }
+            div {
+                div {
+                    div { b { + "Player Hand"} }
+                    div { b { + "cards go here" } }
+                    div { b { + "12 cards" } }
+                }
+            }
         }
     }
 }
