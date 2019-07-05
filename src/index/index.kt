d51977848ba2a400ce6d8c0787fb89b40fa1ee20
buildbro <apeapius@gmail.com>
2019-07-05 08:52:39 +0100

    Add Properties to Tag

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,5 +1,7 @@
 package index
 
+import kotlinx.html.style
+import kotlinx.html.title
 import react.dom.b
 import react.dom.div
 import react.dom.h1
@@ -14,10 +16,21 @@ fun main(args: Array<String>) {
                 + "Blackjack"
             }
             div {
+                attrs.title = "Hands" //shows title when you hover on this div
+                attrs.style = kotlinext.js.js {
+                    display = "flex" //keeps hands side-by-side
+                }
+                
                 div {
                     div { b { + "Player Hand"} }
                     div { b { + "cards go here" } }
                     div { b { + "12 cards" } }
+                }
+
+                div {
+                    div { b { + "Dealer Hand"} }
+                    div { b { + "cards go here" } }
+                    div { b { + "14 cards" } }
                 }
             }
         }
